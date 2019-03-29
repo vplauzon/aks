df65f455f98b4ac2e0018684d87840e247e45486
vplauzon <vplauzon@vplauzon.com>
2019-03-29 10:24:54 -0400

    .

---
+++ requests-vs-limits/CpuRamRequestSolution/CpuRamRequestApi/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CpuRamRequestApi.Controllers
+{
+    [Route("/")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
