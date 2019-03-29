1483d31df6eb092879b741964025c28c5940aa90
vplauzon <vplauzon@vplauzon.com>
2019-03-29 11:28:25 -0400

    Docker build script

---
+++ requests-vs-limits/CpuRamRequestSolution/build-container.sh
@@ -0,0 +1,15 @@
+#   This script isn't used in CI / CD ; it is there for manual testing if needed
+
+#	Build docker container
+sudo docker build -t vplauzon/cpu-ram-request-api:1 .
+
+#	Publish image
+sudo docker push vplauzon/cpu-ram-request-api:1
+
+#	Test image
+sudo docker run --name test-api -d -p 4000:80 vplauzon/cpu-ram-request-api:1
+curl localhost:4000
+#   Get into image
+sudo docker run --name test-api -it vplauzon/cpu-ram-request-api:1 -p 4000:80 bash
+#  Clean up after test
+sudo docker stop test-api && sudo docker container prune -f
